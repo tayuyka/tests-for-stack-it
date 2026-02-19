20345acc3965bb9454f81a0216a2810eb0a59923
yuya <nerentabelen@mail.ru>
2026-02-19 16:32:55 +0300

    add .env into config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
+import path from "node:path";
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
 
 /**
  * Read environment variables from file.
@@ -27,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://demo.app.stack-it.ru/fl/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
