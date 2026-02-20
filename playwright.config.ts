5525308a23031d4b7e33a093dc4cac2669a2e344
yuya <nerentabelen@mail.ru>
2026-02-20 21:02:37 +0300

    set correct base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://demo.app.stack-it.ru/fl/',
+    baseURL: 'https://demo.app.stack-it.ru',
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
     video: 'retain-on-failure',
