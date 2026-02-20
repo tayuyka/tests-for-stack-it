b99d43542294c6db5df514768ca4262456d9026b
yuya <nerentabelen@mail.ru>
2026-02-20 20:01:25 +0300

    minor changes in add district test

--- tests/ui/address-fund.add.spec.ts
+++ tests/ui/address-fund.add.spec.ts
@@ -4,16 +4,16 @@ import {AccountsPage} from "../../pages/AccountsPage";
 
 
 test ('add district @regression', async ({page}) => {
-    const loginPage = new LoginPage(page)
-    const accountPage = new AccountsPage(page)
-    await loginPage.open()
+    const loginPage = new LoginPage(page);
+    const accountPage = new AccountsPage(page);
+    await loginPage.open();
 
-    await loginPage.waitForOpenForm()
+    await loginPage.waitForOpenForm();
     await loginPage.login(process.env.STACK_LOGIN!, process.env.STACK_PASSWORD!);
 
     const districtName: string = `new-district-${Date.now()}`;
 
-    await accountPage.open()
-    await accountPage.addNewDistrict(districtName)
-    await accountPage.checkNoteInTable(districtName)
+    await accountPage.open();
+    const recordNumber: number = await accountPage.addNewDistrict(districtName);
+    await accountPage.checkNoteInTable(recordNumber);
 });
