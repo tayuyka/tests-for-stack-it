376b3be805c4a98f83a306fd50f6416350b3542a
yuya <nerentabelen@mail.ru>
2026-02-20 20:49:36 +0300

    create full e2e test with login -> add district -> change -> delete

---
+++ tests/ui/address-fund.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from "@playwright/test";
+import {LoginPage} from "../../pages/LoginPage";
+import {AccountsPage} from "../../pages/AccountsPage";
+
+test ('District CRUD @regression', async ({page}) => {
+    let recordNumber: number;
+    const districtName: string = `new-district-${Date.now()}`;
+
+    const loginPage = new LoginPage(page);
+    const accountPage = new AccountsPage(page);
+
+    await loginPage.open();
+    await loginPage.waitForOpenForm();
+    await loginPage.login(process.env.STACK_LOGIN!, process.env.STACK_PASSWORD!);
+
+    await accountPage.open();
+    recordNumber = await accountPage.addNewDistrict(districtName);
+    await accountPage.checkNoteInTable(recordNumber);
+
+    const newName = `${districtName}-edited`;
+    await accountPage.editDistrict(recordNumber, newName);
+
+    await accountPage.deleteDistrict(recordNumber);
+});
