93972f3844b20cf2fee427963922ec6c1a9c1e05
yuya <nerentabelen@mail.ru>
2026-02-21 16:59:18 +0300

    add beforeEach with login to add-district tests & create @negative add nameless test

--- tests/ui/address-fund.add.spec.ts
+++ tests/ui/address-fund.add.spec.ts
@@ -3,17 +3,33 @@ import {LoginPage} from "../../pages/LoginPage";
 import {AccountsPage} from "../../pages/AccountsPage";
 
 
-test ('add district @regression', async ({page}) => {
-    const loginPage = new LoginPage(page);
-    const accountPage = new AccountsPage(page);
-    await loginPage.open();
+test.describe('add district @regression', () => {
+    test.beforeEach(async ({page}) => {
+        const loginPage = new LoginPage(page);
 
-    await loginPage.waitForOpenForm();
-    await loginPage.login(process.env.STACK_LOGIN!, process.env.STACK_PASSWORD!);
+        await loginPage.open();
+        await loginPage.waitForOpenForm();
+        await loginPage.login(process.env.STACK_LOGIN!, process.env.STACK_PASSWORD!);
 
-    const districtName: string = `new-district-${Date.now()}`;
+    });
 
-    await accountPage.open();
-    const recordNumber: number = await accountPage.addNewDistrict(districtName);
-    await accountPage.checkNoteInTable(recordNumber);
+    test ('add district @regression', async ({page}) => {
+        const accountPage = new AccountsPage(page);
+        const districtName: string = `new-district-${Date.now()}`;
+
+        await accountPage.open();
+        const recordNumber: number = await accountPage.addNewDistrict(districtName);
+        await accountPage.checkNoteInTable(recordNumber, districtName);
+    });
+
+    test ('add nameless district @regression @negative', async ({page}) => {
+        const accountPage = new AccountsPage(page);
+
+        await accountPage.open();
+        await accountPage.openDistrictForm();
+        await accountPage.stackDialog.nameInput.clear();
+        await accountPage.stackDialog.saveBtn.click();
+
+        await accountPage.stackDialog.expectVisible(/поле не может быть пустым/i);
+    });
 });
